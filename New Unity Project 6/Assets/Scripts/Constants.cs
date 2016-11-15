9974e62aed7cb35a0be88f2e9040982d741b424d
LennysHead <lenny.will@web.de>
2016-11-15 12:00:03 +0100

    Instatiate master branch

---
+++ New Unity Project 6/Assets/Scripts/Constants.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Constants{
+
+    public static int READY = 0;
+    public static int ISACTIVE = 1;
+    public static int ISDEACTIVATED = 2;
+
+
+    public static int WALL = 0;
+    public static int MAZE = 1;
+	
+}
